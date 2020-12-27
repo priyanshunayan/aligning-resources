8880c2446eb73fc5247d90130784de2441115ed2
Priyanshu Nayan <priyanshunayan456@gmail.com>
2020-12-27 20:01:47 +0530

    GET, PUT, POST without express

--- app.js
+++ app.js
@@ -20,24 +20,74 @@ const server = http.createServer((req, res) => {
 	if(url == '/api' && method == "PUT"){
 		editResource(res);
 	}
-	// res.statusCode = 200;
-	// res.setHeader('Content-Type', 'text/plain');
-	// res.end("Hello World \n");
 });
 
+const readData = (fd, res) => {
+	const readStream = fs.createReadStream("", {fd: fd});
+	let output = "";
+	readStream.on('data', function(chunk) {
+		output += chunk;
+	});
+
+	readStream.on('end', function() {
+		res.statusCode = 200;
+		res.statusMessage = "Read successful";
+		res.setHeader('Content-Type', 'text/json');
+		res.write(JSON.stringify({
+			"message": output,
+		}));
+		res.end(() => {
+			console.log("ReadData function reads: ", output);
+		})
+		
+	});
+
+	readStream.on('error', function(err) {
+		res.statusCode = 500;
+		res.setHeader('Content-Type', 'text/plain');
+		res.end("Error");
+		throw err;
+	});	
+} 
+
+
+const writeData = (fd, data, res) => {
+	
+	fs.writeFile('resource', data, (err) => {
+		if(err) {
+			res.statusCode = 500;
+			res.setHeader('Content-Type', 'text/plain');
+			res.end("Error");
+		}
+		res.statusCode = 200;
+		res.statusMessage = "Read successful";
+		res.setHeader('Content-Type', 'text/json');
+		res.write(JSON.stringify({
+			"message": data,
+		}))
+		res.end(() => {
+			console.log("Written Successfully in the file");
+		})
+		
+	})	
+}
 
 const getResource = (res) => {
 	/* check if the resource exists or in our case, resource.json exists
 		if yes then return the resource else throw a 404 error
 	*/
 
-	fs.exists('resource.json', (exists) => {
-		if(exists){
-			console.log("returning the json of the file");
-		}else{
-			console.log("forbidden");
+	fs.open('resource', 'r', (err, fd) => {
+		if(err){
+			if(err.code == 'ENOENT'){
+				res.statusCode = 500;
+				res.setHeader('Content-Type', 'text/plain');
+				res.end("Error");
+			}
+		throw err;
 		}
-	})
+		readData(fd, res);
+	});
 
 	console.log("GET is called");
 
@@ -47,7 +97,15 @@ const createResource = (res) => {
 	/* check if the resource exists or in our case, resource.json exists
 		if not then create it with dummy data, else return forbidden header
 	*/
-	
+	fs.open('resource', 'wx', (err, fd) => {
+		if(err){
+			if(err.code == 'EEXIST'){
+				console.error("File already exists");
+			}
+		throw err;
+		}
+		writeData(fd, 'Hello there, I am a resource \n', res);
+	});
 	console.log("POST is called");
 }
 
@@ -55,7 +113,12 @@ const editResource = (res) => {
 	/* check if the resource exists or in our case, resource.json exists
 		if yes then edit the resource with modified value else throw a 404 error
 	*/
-	
+	fs.open('resource', 'r', (err, fd) => {
+		if(err){
+			throw err;
+		}
+		writeData(fd, 'Hello there, I am a resource changed \n', res);
+	});
 	console.log("PUT is called");
 
 
